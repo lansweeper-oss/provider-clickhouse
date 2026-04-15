7ea958ebae3fca969d3a0ee1e3845180ee178384
J. Fernández <7312236+fernandezcuesta@users.noreply.github.com>
2026-04-15 13:24:26 +0200

    feat: initial release (#1)
    * chore: initial release
    
    * chore: missing files
    
    * fixup
    
    * fix: lint and yet another missing file
    
    * chore: add crossplane.yaml package metadata
    
    Include provider metadata needed for xpkg build.
    
    Signed-off-by: Jesús Fernández <7312236+fernandezcuesta@users.noreply.github.com>
    
    * chore: remove e2e
    
    * fix: schema-version-diff from raw repo
    
    * chore: run build-test after lint to reuse go cache
    
    * chore: bump up go dependencies
    
    * chore: add platform in docker base image
    
    * fixup
    
    Signed-off-by: Jesús Fernández <7312236+fernandezcuesta@users.noreply.github.com>
    
    * fixup
    
    ---------
    
    Signed-off-by: Jesús Fernández <7312236+fernandezcuesta@users.noreply.github.com>

---
+++ apis/namespaced/v1alpha1/doc.go
@@ -0,0 +1,5 @@
+// Package v1alpha1 contains the core resources of the clickhouse jet provider.
+// +kubebuilder:object:generate=true
+// +groupName=clickhouse.m.crossplane.io
+// +versionName=v1alpha1
+package v1alpha1
---
+++ internal/controller/namespaced/zz_setup.go
@@ -0,0 +1,51 @@
+// SPDX-FileCopyrightText: 2026 The Crossplane Authors <https://crossplane.io>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package controller
+
+import (
+	ctrl "sigs.k8s.io/controller-runtime"
+
+	"github.com/crossplane/upjet/v2/pkg/controller"
+
+	service "github.com/lansweeper-oss/provider-clickhouse/internal/controller/namespaced/clickhouse/service"
+	settings "github.com/lansweeper-oss/provider-clickhouse/internal/controller/namespaced/organization/settings"
+	providerconfig "github.com/lansweeper-oss/provider-clickhouse/internal/controller/namespaced/providerconfig"
+	privateendpointsattachment "github.com/lansweeper-oss/provider-clickhouse/internal/controller/namespaced/service/privateendpointsattachment"
+	transparentdataencryptionkeyassociation "github.com/lansweeper-oss/provider-clickhouse/internal/controller/namespaced/service/transparentdataencryptionkeyassociation"
+)
+
+// Setup creates all controllers with the supplied logger and adds them to
+// the supplied manager.
+func Setup(mgr ctrl.Manager, o controller.Options) error {
+	for _, setup := range []func(ctrl.Manager, controller.Options) error{
+		service.Setup,
+		settings.Setup,
+		providerconfig.Setup,
+		privateendpointsattachment.Setup,
+		transparentdataencryptionkeyassociation.Setup,
+	} {
+		if err := setup(mgr, o); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// SetupGated creates all controllers with the supplied logger and adds them to
+// the supplied manager gated.
+func SetupGated(mgr ctrl.Manager, o controller.Options) error {
+	for _, setup := range []func(ctrl.Manager, controller.Options) error{
+		service.SetupGated,
+		settings.SetupGated,
+		providerconfig.SetupGated,
+		privateendpointsattachment.SetupGated,
+		transparentdataencryptionkeyassociation.SetupGated,
+	} {
+		if err := setup(mgr, o); err != nil {
+			return err
+		}
+	}
+	return nil
+}

2f045fd56a4a11a9babaa97bba06bdc5f9ef07ba
Jeroen Savat <jeroen.savat@lansweeper.com>
2026-05-07 13:49:27 +0200

    feat: add autogen pwd case

--- apis/cluster/clickhouse/v1alpha1/zz_service_types.go
+++ apis/cluster/clickhouse/v1alpha1/zz_service_types.go
@@ -344,7 +344,7 @@ type ServiceInitParameters struct {
 	PasswordHashSecretRef *v1.SecretKeySelector `json:"passwordHashSecretRef,omitempty" tf:"-"`
 
 	// (String, Sensitive) Password for the default user. One of either password, password_wo, or password_hash must be specified.
-	// Password for the default user. One of either `password`, `password_wo`, or `password_hash` must be specified.
+	// Password for the default ClickHouse user. When passwordSecretRef is set, that secret is used (Bring Your Own Password). Otherwise a password is auto-generated and written to writeConnectionSecretToRef.
 	PasswordSecretRef *v1.SecretKeySelector `json:"passwordSecretRef,omitempty" tf:"-"`
 
 	// only, not persisted to state).
@@ -608,7 +608,7 @@ type ServiceParameters struct {
 	PasswordHashSecretRef *v1.SecretKeySelector `json:"passwordHashSecretRef,omitempty" tf:"-"`
 
 	// (String, Sensitive) Password for the default user. One of either password, password_wo, or password_hash must be specified.
-	// Password for the default user. One of either `password`, `password_wo`, or `password_hash` must be specified.
+	// Password for the default ClickHouse user. When passwordSecretRef is set, that secret is used (Bring Your Own Password). Otherwise a password is auto-generated and written to writeConnectionSecretToRef.
 	// +kubebuilder:validation:Optional
 	PasswordSecretRef *v1.SecretKeySelector `json:"passwordSecretRef,omitempty" tf:"-"`
 
---
+++ config/common/password.go
@@ -0,0 +1,142 @@
+// Package common holds shared resource configuration helpers.
+package common
+
+import (
+	"context"
+	"fmt"
+
+	v1 "github.com/crossplane/crossplane-runtime/v2/apis/common/v1"
+	"github.com/crossplane/crossplane-runtime/v2/pkg/fieldpath"
+	"github.com/crossplane/crossplane-runtime/v2/pkg/meta"
+	"github.com/crossplane/crossplane-runtime/v2/pkg/password"
+	xpresource "github.com/crossplane/crossplane-runtime/v2/pkg/resource"
+	"github.com/crossplane/upjet/v2/pkg/config"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	"github.com/crossplane/crossplane-runtime/v2/pkg/reconciler/managed"
+)
+
+const passwordKey = "password"
+
+// ClusterPasswordSecretRefSetter is implemented by cluster-scoped managed
+// resources that expose a *v1.SecretKeySelector PasswordSecretRef.
+type ClusterPasswordSecretRefSetter interface {
+	SetPasswordSecretRef(ref *v1.SecretKeySelector)
+}
+
+// NamespacedPasswordSecretRefSetter is implemented by namespaced managed
+// resources that expose a *v1.LocalSecretKeySelector PasswordSecretRef.
+type NamespacedPasswordSecretRefSetter interface {
+	SetPasswordSecretRef(ref *v1.LocalSecretKeySelector)
+}
+
+// PasswordGenerator returns a NewInitializerFn:
+//   - BYOP: if byopSecretRefPath is already set, no-op.
+//   - Auto-generate: if writeConnectionSecretPath is set, generate a password,
+//     write it to that secret under "password", and point passwordSecretRef at it.
+func PasswordGenerator(byopSecretRefPath, writeConnectionSecretPath string) config.NewInitializerFn {
+	return func(cl client.Client) managed.Initializer {
+		return managed.InitializerFn(func(ctx context.Context, mg xpresource.Managed) error {
+			paved, err := fieldpath.PaveObject(mg)
+			if err != nil {
+				return fmt.Errorf("cannot pave object: %w", err)
+			}
+			byop, err := checkBYOP(paved, byopSecretRefPath)
+			if err != nil {
+				return err
+			}
+			if byop {
+				return nil
+			}
+			name, ns, err := resolveConnRef(paved, writeConnectionSecretPath, mg.GetNamespace())
+			if err != nil || name == "" {
+				return err
+			}
+			return reconcilePassword(ctx, cl, mg, name, ns)
+		})
+	}
+}
+
+func checkBYOP(paved *fieldpath.Paved, path string) (bool, error) {
+	sel := map[string]any{}
+	if err := paved.GetValueInto(path, &sel); err != nil {
+		if fieldpath.IsNotFound(err) {
+			return false, nil
+		}
+		return false, fmt.Errorf("cannot read %s: %w", path, err)
+	}
+	name, _ := sel["name"].(string)
+	return name != "", nil
+}
+
+func resolveConnRef(paved *fieldpath.Paved, path, defaultNS string) (name, ns string, err error) {
+	ref := map[string]any{}
+	if err := paved.GetValueInto(path, &ref); err != nil {
+		if fieldpath.IsNotFound(err) {
+			return "", "", nil
+		}
+		return "", "", fmt.Errorf("cannot read %s: %w", path, err)
+	}
+	name, _ = ref["name"].(string)
+	ns, _ = ref["namespace"].(string)
+	if ns == "" {
+		ns = defaultNS
+	}
+	return name, ns, nil
+}
+
+func reconcilePassword(ctx context.Context, cl client.Client, mg xpresource.Managed, name, ns string) error {
+	s := &corev1.Secret{}
+	getErr := cl.Get(ctx, types.NamespacedName{Namespace: ns, Name: name}, s)
+	if xpresource.IgnoreNotFound(getErr) != nil {
+		return fmt.Errorf("cannot get connection secret: %w", getErr)
+	}
+	if getErr == nil && len(s.Data[passwordKey]) != 0 {
+		return setPasswordSecretRef(ctx, cl, mg, name, ns)
+	}
+	return generateAndApply(ctx, cl, mg, s, name, ns)
+}
+
+func generateAndApply(ctx context.Context, cl client.Client, mg xpresource.Managed, s *corev1.Secret, name, ns string) error {
+	pw, err := password.Generate()
+	if err != nil {
+		return fmt.Errorf("cannot generate password: %w", err)
+	}
+	s.SetName(name)
+	s.SetNamespace(ns)
+	s.Type = xpresource.SecretTypeConnection
+	if !meta.WasCreated(s) {
+		meta.AddOwnerReference(s, meta.AsController(meta.TypedReferenceTo(mg, mg.GetObjectKind().GroupVersionKind())))
+	}
+	if s.Data == nil {
+		s.Data = make(map[string][]byte, 1)
+	}
+	s.Data[passwordKey] = []byte(pw)
+	if err := xpresource.NewAPIPatchingApplicator(cl).Apply(ctx, s); err != nil {
+		return fmt.Errorf("cannot apply password secret: %w", err)
+	}
+	return setPasswordSecretRef(ctx, cl, mg, name, ns)
+}
+
+func setPasswordSecretRef(ctx context.Context, cl client.Client, mg xpresource.Managed, name, namespace string) error {
+	switch setter := mg.(type) {
+	case ClusterPasswordSecretRefSetter:
+		setter.SetPasswordSecretRef(&v1.SecretKeySelector{
+			SecretReference: v1.SecretReference{Name: name, Namespace: namespace},
+			Key:             passwordKey,
+		})
+	case NamespacedPasswordSecretRefSetter:
+		setter.SetPasswordSecretRef(&v1.LocalSecretKeySelector{
+			LocalSecretReference: v1.LocalSecretReference{Name: name},
+			Key:                  passwordKey,
+		})
+	default:
+		return nil
+	}
+	if err := cl.Update(ctx, mg); err != nil {
+		return fmt.Errorf("cannot update managed resource with password secret ref: %w", err)
+	}
+	return nil
+}
